8eb483d70eeb74f97238b2f5c099d27ca8545e00
Jeff Morris <jeffreylee3@gmail.com>
2013-04-29 15:08:26 -0500

    updated main

--- main.rb
+++ main.rb
@@ -4,9 +4,9 @@ require_relative 'tic_tac_toe'
 require_relative 'minimax'
 
 
-# checks validity of human player input
-# infinity loop for invalid values
-# breaks and returns input for valid entry
+#
+# checks and accepts player input value
+#
 def valid_move(curr_player, avail_moves)
   input = ""
 
@@ -14,92 +14,110 @@ def valid_move(curr_player, avail_moves)
     print "#{curr_player.name}, please input move: "
     input = gets.chomp
     break if input.downcase == "quit" || avail_moves.include?(input.to_i)
-    puts
     puts "Please input an available move: #{avail_moves} or ['quit']"
-    puts
   end
 
   return input
 end
 
-# processes a player turn for both a human
-# and cpu player
-# returns "quit", "winner", and "draw" for
-# end game notification
+
+# 
+# processes player turns
+# returns "quit", "winner", and "draw" for endgame
+# 
 def player_turn(curr_player, opponent, ttt)
   avail_moves = ttt.open_squares(opponent.moves, curr_player.moves)
   
-  if curr_player.cpu == true                                            # CPU turn processing
-    puts "CPU is thinking..."
-    moves = [opponent.moves, curr_player.moves]
-    node = Minimax.new moves, avail_moves
+  #
+  # CPU player processing
+  #
+  if curr_player.cpu == true                                            
+    node = Minimax.new [opponent.moves, curr_player.moves], avail_moves
     node.create_minimax_tree(ttt)
-
     next_move = node.select_computer_move
     input = next_move[0]
     puts "CPU Turn: #{input}"
-  else                                                                  # Human player processing
+  else  # Human player processing
     input = valid_move(curr_player, avail_moves)
     return "quit" if input.downcase == "quit"
   end
 
-  #ttt.turn(curr_player, input.to_i)                                     # records move
+  #
+  # record and display move
+  #
   ttt.mark_board(input.to_i, curr_player.mark)
   curr_player.move(input.to_i)
-  ttt.display                                                           # displays updated board
-  if ttt.winner?(curr_player.moves)                                     # evaluates for winning move
+  ttt.display     
+
+  #
+  # check for winner or draw status
+  # 
+  if ttt.winner?(curr_player.moves)                                    
     puts "#{curr_player.name} wins!!"
     return "winner"
-  elsif ttt.draw?(curr_player.moves, opponent.moves)                    # evaluates for draw
+  elsif ttt.draw?(curr_player.moves, opponent.moves)                    
     puts "Game ends in draw."
     return "draw"
   end
 end
 
 
+#
+# initialize gameboard
+#
+ttt = TicTacToe.new 9, 3
 
-ttt = TicTacToe.new 9, 3                                              # create tic tac toe game board object
 
-ttt.instructions                                                      # display instructions
+#
+# display instructions  
+#                                            
+ttt.instructions                                                      
 
-# create player 1 object
-p1 = Player.new "X"
-print "Player 1, enter name: "
-p1.name = gets.chomp
+
+#
+# player setup
+#
 print "Would you like to go first (y/n): "
-gets.chomp == "y" ? p1.first_to_act = true : p1.first_to_act = false
-puts
+if gets.chomp == "y"
+  p1 = Player.new "X"
+  print "Player 1, enter name: "
+  p1.name = gets.chomp
 
-# create player 2 object
-p2 = Player.new "O"
-p2.name = "cpu"
-p2.cpu = true
+  p2 = Player.new "O", "cpu", true
+else
+  p2 = Player.new "X"
+  print "Player 1, enter name: "
+  p2.name = gets.chomp
+
+  p1 = Player.new "O", "cpu", true
+end
 
 
+#
+# game loop
+#
+loop do                                                               
 
-loop do                                                               # infinity move loop
-
-  # if else statements check to see who turn it is,
-  # then set respective "curr_player_turn" and
-  # "not_player_turn" values
-  if ttt.open_squares(p1.moves, p2.moves).length.odd? && p1.first_to_act == true
+  # 
+  # determin whose turn it is
+  # odd num of open squares => p1 turn
+  # even num of open squares => p2 turn
+  #
+  if ttt.open_squares(p1.moves, p2.moves).length.odd?
     curr_player_turn = p1
-    not_player_turn = p2
-  elsif ttt.open_squares(p1.moves, p2.moves).length.even? && p1.first_to_act == true
+    opponent = p2 
+  else ttt.open_squares(p1.moves, p2.moves).length.even? 
     curr_player_turn = p2
-    not_player_turn = p1   
-  elsif ttt.open_squares(p1.moves, p2.moves).length.odd? && p1.first_to_act == false
-    curr_player_turn = p2
-    not_player_turn = p1   
-  elsif ttt.open_squares(p1.moves, p2.moves).length.even? && p1.first_to_act == false
-    curr_player_turn = p1
-    not_player_turn = p2  
+    opponent = p1      
   end  
 
-  game_result = player_turn(curr_player_turn, not_player_turn, ttt) 
-  break if game_result == "quit" || game_result == "winner" || game_result == "draw"   
-
+  turn_result = player_turn(curr_player_turn, opponent, ttt) 
+  break if turn_result == "quit" || turn_result == "winner" || turn_result == "draw"   
 end
+
+
+
+
 
 
 
--- player.rb
+++ player.rb
@@ -2,12 +2,12 @@ class Player
   attr_accessor :mark, :cpu, :name, :first_to_act
   attr_reader :moves
 
-  def initialize(mark)
+  def initialize(mark, name="",cpu=false)
     @mark = mark                                        # game piece, 'X' and 'O' for tic tac toe
-    @cpu = false                                        # true = this is a cpu player, false = this is a human player
+    @cpu = cpu                                        # true = this is a cpu player, false = this is a human player
     @first_to_act = false                               # true = first player to move, false = second player to move
     @moves = []
-    @name = "" 
+    @name = name 
   end
 
   #
